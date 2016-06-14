9f403485d6640a419614255b04d89b5ea2f9865a
mingong <gongmingqm10@foxmail.com>
2016-06-15 00:11:11 +0100

    Add the beginner navigatorIOS

---
+++ react-native/githubNotetaker/index.ios.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} from 'react-native';
+
+var Main = require('./App/Components/Main');
+
+class githubNotetaker extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          component: Main,
+          title: 'Github Notetaker'
+        }}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111111'
+  }
+});
+
+AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
