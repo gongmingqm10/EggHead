0dca7871b94fb9b7b0a4840cd147cdf3e1fa86ed
mingong <gongmingqm10@foxmail.com>
2016-06-16 08:40:02 +0100

    React Native: Add API to create and list notes from Firebase

--- react-native/githubNotetaker/App/Utils/api.js
+++ react-native/githubNotetaker/App/Utils/api.js
@@ -8,6 +8,19 @@ var api = {
     username = username.toLowerCase().trim();
     var url = 'https://api.github.com/users/' + username;
     return fetch(url).then((res) => res.json());
+  },
+  getNotes(username) {
+    username = username.toLowerCase().trim();
+    var url = "https://github-saver-55b36.firebaseio.com/" + username + ".json";
+    return fetch(url).then((res) => res.json());
+  },
+  addNotes(username, note) {
+    username = username.toLowerCase().trim();
+    var url = "https://github-saver-55b36.firebaseio.com/" + username + ".json";
+    return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(note)
+    }).then((res) => res.json());
   }
 };
 
