e90b4294f3a8d2196f61c93eaeb8e245cc5269ee
mingong <gongmingqm10@foxmail.com>
2016-06-21 10:19:43 +0100

    React Fundamentals: Using refs to access components

--- react-fundamentals/es6-react-setup/App.js
+++ react-fundamentals/es6-react-setup/App.js
@@ -3,28 +3,44 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
-
     this.state = {
-      txt: 'Text from the state',
-      cat: 0
+      red: 128,
+      green: 128,
+      blue: 128
     };
+    this.update = this.update.bind(this);
   }
 
   update(e) {
-    this.setState({txt: e.target.value})
+    this.setState({
+      red: this._red.refs.inputSlider.value,
+      green: this._green.refs.inputSlider.value,
+      blue: this._blue.refs.inputSlider.value
+    });
   }
 
   render() {
     return (
       <div>
-        <h1>Hello React World </h1>
-        <p>This will show the value of txt: {this.props.txt}</p>
-        <p>Hello State: {this.state.txt} </p>
-
-        <input type="text" onChange={this.update.bind(this)} />
-
+        <h1> Color Palette </h1>
+        <Slider ref={(ref) => this._red  = ref} txt={this.state.red} update={this.update} />
+        <br/>
+        <Slider ref={(ref) => this._green  = ref} txt={this.state.green} update={this.update} />
+        <br/>
+        <Slider ref={(ref) => this._blue  = ref} txt={this.state.blue} update={this.update} />
       </div>
     );
+  }
+}
+
+class Slider extends React.Component {
+  render() {
+    return(
+      <div>
+        <input ref="inputSlider" type="range" min="0" max="255" onChange={this.props.update} />
+        <span> {this.props.txt}</span>
+      </div>
+    )
   }
 }
 
