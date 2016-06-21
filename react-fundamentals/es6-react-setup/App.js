09b61a2670af0b7d4253676cbb4d0f48f58bfdd2
mingong <gongmingqm10@foxmail.com>
2016-06-21 09:23:15 +0100

    React fundamentals: State Basics

--- react-fundamentals/es6-react-setup/App.js
+++ react-fundamentals/es6-react-setup/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
 class App extends React.Component {
-  render() {
-    return <div> Hello React World </div>
+  constructor() {
+    super();
+
+    this.state = {
+      txt: 'Text from the state',
+      cat: 0
+    };
   }
+
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello React World </h1>
+        <p>This will show the value of txt: {this.props.txt}</p>
+        <p>Hello State: {this.state.txt} </p>
+
+        <input type="text" onChange={this.update.bind(this)} />
+
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'gongmingqm10'
 }
 
 export default App
