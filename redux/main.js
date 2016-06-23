b575c4c5611b4aa453f548007f43d487ac554bf8
mingong <gongmingqm10@foxmail.com>
2016-06-23 16:50:23 +0100

    Redux: Store Methods: getState(), dispatch() and subscribe()

--- redux/main.js
+++ redux/main.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+
+// This is the callback for store
+// whenver there is an dispatcher event.
+store.subscribe(() => {
+  document.body.innerText = store.getState();
+});
+
+document.addEventListener('click', () => {
+  store.dispatch({type: 'INCREMENT'});
+});
