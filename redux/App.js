84a6138c21bb53bcb196b034be17cae844813966
mingong <gongmingqm10@foxmail.com>
2016-06-23 16:24:07 +0100

    Redux: Setup basic project

---
+++ redux/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <h1>Hello Redux</h1>
+    )
+  }
+
+}
+
+export default App;
