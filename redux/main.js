05bba0462a1fc7a8000d614c14538809ea4b5f27
mingong <gongmingqm10@foxmail.com>
2016-06-24 10:40:03 +0100

    Redux: Extract subscribe method

--- redux/main.js
+++ redux/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore} from 'redux';
+// import {createStore} from 'redux';
 
 // ReactDOM.render(<App />, document.getElementById('app'));
 
@@ -16,13 +16,44 @@ const counter = (state = 0, action) => {
   }
 }
 
+const createStore = (render) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const   = (action) => {
+    state = render(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    }
+  };
+
+  dispatch({});
+
+  return {getState, dispatch, subscribe};
+}
+
 const store = createStore(counter);
 
 // This is the callback for store
 // whenver there is an dispatcher event.
-store.subscribe(() => {
+const render = () => {
   document.body.innerText = store.getState();
-});
+  if (store.getState() === 10) {
+    // Unsubscribe!!
+    subsciption();
+  }
+};
+
+var subsciption = store.subscribe(render);
+
+render();
 
 document.addEventListener('click', () => {
   store.dispatch({type: 'INCREMENT'});
