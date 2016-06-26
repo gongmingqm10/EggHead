12cc2caed6377a6552a9bd0fc8cf427d8ee35031
mingong <gongmingqm10@foxmail.com>
2016-06-26 15:10:18 +0100

    Redux: Extracting TodoApp Component

--- redux/TodoApp.js
+++ redux/TodoApp.js
@@ -1,6 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const filters = [
+  {text: 'All', filter: 'SHOW_ALL'},
+  {text: 'Completed', filter: 'SHOW_COMPLETED'},
+  {text: 'Active', filter: 'SHOW_UNCOMPLETED'}
+];
+
+class Todo extends React.Component {
+  render() {
+    return(
+      <li onClick={this.props.onClick}
+        style={{cursor: 'pointer', textDecoration: this.props.completed ? 'line-through' : 'none'}}
+        >
+        {this.props.text}
+      </li>
+    )
+  }
+}
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul style={styles.todoList}>
+        {this.props.todos.map(todo =>
+          <Todo
+            key={todo.id}
+            onClick={this.props.onToggleTodo.bind(this, todo.id)}
+            {...todo}
+            />
+        )}
+      </ul>
+    )
+  }
+}
+
+class FilterView extends React.Component {
+  render() {
+    return (
+      <div style={styles.filterContainer}>
+        Show: {filters.map(item =>
+          <FilterItem
+            {...item}
+            key={item.filter}
+            onClick={this.props.onVisibilityFilter.bind(this, item.filter)}
+            selected={this.props.visibilityFilter === item.filter} />
+        )}
+      </div>
+    )
+  }
+}
+
+class FilterItem extends React.Component {
+  render() {
+    return (
+      <span
+        style={{
+          cursor: 'pointer',
+          marginRight: 8,
+          textDecoration: this.props.selected ? 'underline':'none',
+          color: this.props.selected ? 'blue': 'black'}}
+        onClick={this.props.onClick}>
+        {this.props.text}
+      </span>
+    )
+  }
+}
+
 class TodoApp extends React.Component {
   constructor(props) {
     super(props);
@@ -52,17 +118,6 @@ class TodoApp extends React.Component {
       )
     });
 
-    const todoList = this.getVisibleTodos(this.props.todos, this.props.visibilityFilter).map((todo) => {
-      return (
-        <li
-          onClick = {this.props.onToggleTodo.bind(this, todo.id)}
-          style={{cursor: 'pointer', textDecoration: todo.completed ? 'line-through' : 'none'}}
-          key={todo.id}>
-          {todo.text}
-        </li>
-      )
-    });
-
     return (
       <div style={styles.todoContainer}>
         <div>
@@ -70,10 +125,13 @@ class TodoApp extends React.Component {
           <button style={styles.button} onClick={this.addTodo}>Add Todo</button>
         </div>
 
-        <ul style={styles.todoList}>
-          {todoList}
-        </ul>
-        <div style={styles.filterContainer}>Show: {filterView}</div>
+        <TodoList
+          onToggleTodo = {this.props.onToggleTodo}
+          todos={this.getVisibleTodos(this.props.todos, this.props.visibilityFilter)}/>
+
+        <FilterView
+          onVisibilityFilter={this.props.onVisibilityFilter}
+          visibilityFilter={this.props.visibilityFilter} />
       </div>
     )
   }
@@ -94,7 +152,7 @@ const styles = {
   filterContainer: {
     flexDirection: 'row'
   }
-}
+};
 
 
 export default TodoApp;
